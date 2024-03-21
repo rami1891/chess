148edcd95012d4c86bda73af997ceb6b468c3eb1
Rami Arafeh <ramiarafeh@Ramis-MBP.app.byu.edu>
2024-03-20 18:45:13 -0600

    tests

--- client/src/test/java/clientTests/ServerFacadeTests.java
+++ client/src/test/java/clientTests/ServerFacadeTests.java
@@ -30,9 +30,66 @@ public class ServerFacadeTests {
 
 
     @Test
-    public void testRegister() throws ResponseException {
-        facade.register("user1", "password", "email");
+    public void testRegisterPositive() throws ResponseException {
+
+        Assertions.assertDoesNotThrow(() -> facade.register("user1", "password", "email"));
     }
+
+    @Test
+    public void testRegisterNegative() throws ResponseException {
+        facade.register("user1", "password", "email");
+        Assertions.assertThrows(ResponseException.class, () -> facade.register("user1", "password", "email"));
+
+    }
+
+    @Test
+    public void testLoginPositive() throws ResponseException {
+        facade.register("user1", "password", "email");
+        Assertions.assertDoesNotThrow(() -> facade.login("user1", "password"));
+    }
+
+    @Test
+    public void testLoginNegative() throws ResponseException {
+        facade.register("user1", "password", "email");
+
+        Assertions.assertThrows(ResponseException.class, () -> facade.login("user2", "password"));
+    }
+
+    @Test
+    public void testLogoutPositive() throws ResponseException {
+        facade.register("user1", "password", "email");
+        facade.login("user1", "password");
+        Assertions.assertDoesNotThrow(() -> facade.logout());
+    }
+
+    @Test
+    public void testLogoutNegative() throws ResponseException {
+        facade.register("user1", "password", "email");
+        facade.logout();
+        Assertions.assertThrows(ResponseException.class, () -> facade.logout());
+    }
+
+    @Test
+    public void testCreateGamePositive() throws ResponseException {
+        facade.register("user1", "password", "email");
+        facade.login("user1", "password");
+        Assertions.assertDoesNotThrow(() -> facade.createGame("game1"));
+    }
+
+    @Test
+    public void testCreateGameNegative() throws ResponseException {
+        facade.register("user1", "password", "email");
+        facade.login("user1", "password");
+        facade.createGame("game1");
+        Assertions.assertThrows(ResponseException.class, () -> facade.createGame("game1"));
+    }
+
+
+
+
+
+
+
 
 
 
